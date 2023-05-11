b0e9d16f9499e250ba9e6ae1b72f2bdcc80421f4
JustinMassart <justin.massart@student.hepl.be>
2023-05-11 17:01:53 +0200

    Updated condition to Language screen

--- App/Navigation/index.tsx
+++ App/Navigation/index.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomePage from 'app/Pages/Home';
 import LanguagePage from 'app/Pages/Language';
 import {NavigationContainer} from '@react-navigation/native';
-import i18n from 'app/Services/i18n';
 import {AVAILABLE_LANGUAGES, getDefaultLanguage} from 'app/Config/Languages';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createStackNavigator();
 
 const Navigator = () => {
+  const [storageLocale, setStorageLocale] = useState<string | null>(null);
   const defaultLang = getDefaultLanguage();
 
   const isLocaleAvailable = () => {
-    if (AVAILABLE_LANGUAGES.includes(defaultLang)) {
+    if (
+      AVAILABLE_LANGUAGES.includes(defaultLang) ||
+      AVAILABLE_LANGUAGES.includes(storageLocale)
+    ) {
+      console.log(defaultLang);
+      console.log(storageLocale);
       return true;
     }
     return false;
   };
+
+  useEffect(() => {
+    const getStoredLocale = async () => {
+      const storedLocale = await AsyncStorage.getItem('locale');
+      setStorageLocale(storedLocale);
+    };
+    getStoredLocale();
+  });
 
   return (
     <NavigationContainer>
