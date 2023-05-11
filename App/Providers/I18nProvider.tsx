ebda6ad5baa42a0bf74ccb79ca0edb72c4c13465
JustinMassart <justin.massart@student.hepl.be>
2023-05-11 14:01:39 +0200

    Updated i18n provider to fix language problems

--- App/Providers/I18nProvider.tsx
+++ App/Providers/I18nProvider.tsx
@@ -3,9 +3,10 @@ import {initReactI18next} from 'react-i18next';
 import dayjs from 'dayjs';
 
 import i18n, {resources} from 'app/Services/i18n';
+import {getDefaultLanguage} from 'app/Config/Languages';
 
 const I18nProvider = ({children}: {children: JSX.Element}) => {
-  const language = 'en';
+  const language = getDefaultLanguage();
   useEffect(() => {
     i18n.use(initReactI18next).init({
       resources,
