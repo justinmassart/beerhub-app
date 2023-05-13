d464b1c367ded04f1df49e0a36c93e651720aeb3
JustinMassart <justin.massart@student.hepl.be>
2023-05-13 15:47:23 +0200

    Updated HeaderTitle to be black font

--- App/Components/Molecules/Navigation/HeaderTitle.tsx
+++ App/Components/Molecules/Navigation/HeaderTitle.tsx
@@ -1,26 +1,18 @@
-import React, {useContext} from 'react';
+import React from 'react';
 
 import Text from 'app/Components/Atoms/Text';
 import {Colors} from 'app/Themes';
-import {ThemeContext} from 'app/Themes/Styled';
 
 const HeaderTitle = ({
   title,
-  themed,
   tintColor,
 }: {
   title: string;
   themed?: boolean;
   tintColor?: string;
 }) => {
-  const theme = useContext(ThemeContext);
-
   return (
-    <Text
-      centered
-      color={
-        tintColor || (themed ? theme.colors.primary : Colors.common.white)
-      }>
+    <Text centered color={tintColor || Colors.common.black}>
       {title}
     </Text>
   );
