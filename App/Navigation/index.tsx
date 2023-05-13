63047d46d6e150dc571c2206b7d81f6c2bc3b34a
JustinMassart <justin.massart@student.hepl.be>
2023-05-13 15:44:25 +0200

    Fixed Language initial route

--- App/Navigation/index.tsx
+++ App/Navigation/index.tsx
@@ -1,48 +1,50 @@
 import React, {useEffect, useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import HomePage from 'app/Pages/Home';
 import LanguagePage from 'app/Pages/Language';
 import {NavigationContainer} from '@react-navigation/native';
-import {AVAILABLE_LANGUAGES, getDefaultLanguage} from 'app/Config/Languages';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Stack = createStackNavigator();
+import HomeStack from 'app/Navigation/AppTab/HomeStack';
+
+const RootStack = createStackNavigator();
 
 const Navigator = () => {
-  const [storageLocale, setStorageLocale] = useState<string | null>(null);
-  const defaultLang = getDefaultLanguage();
-
-  const isLocaleAvailable = () => {
-    if (
-      AVAILABLE_LANGUAGES.includes(defaultLang) ||
-      AVAILABLE_LANGUAGES.includes(storageLocale)
-    ) {
-      console.log(defaultLang);
-      console.log(storageLocale);
-      return true;
-    }
-    return false;
-  };
+  const [initialRoute, setInitialRoute] = useState<'Home' | 'Language'>();
 
   useEffect(() => {
-    const getStoredLocale = async () => {
+    const checkStoredLocale = async () => {
       const storedLocale = await AsyncStorage.getItem('locale');
-      setStorageLocale(storedLocale);
+      console.log(storedLocale);
+
+      if (storedLocale) {
+        setInitialRoute('Home');
+      } else {
+        setInitialRoute('Language');
+      }
     };
-    getStoredLocale();
-  });
+
+    checkStoredLocale();
+  }, []);
+
+  if (initialRoute === undefined) {
+    return null;
+  }
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={isLocaleAvailable() ? 'Home' : 'Language'}>
-        <Stack.Screen
-          name="Home"
-          component={HomePage}
-          options={{headerLeft: _disabled => true}}
-        />
-        <Stack.Screen name="Language" component={LanguagePage} />
-      </Stack.Navigator>
+      <RootStack.Navigator
+        initialRouteName={initialRoute}
+        screenOptions={{
+          // TODO: Remove detachPreviousScreen
+          // https://github.com/react-navigation/react-navigation/issues/9891#issuecomment-916453157
+          detachPreviousScreen: false,
+          animationEnabled: false,
+          headerShown: false,
+          presentation: 'modal',
+        }}>
+        <RootStack.Screen name="Home" component={HomeStack} />
+        <RootStack.Screen name="Language" component={LanguagePage} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
