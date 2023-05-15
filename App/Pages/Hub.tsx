f86e58da0a90b5f9934e127601083a62c6d61703
JustinMassart <justin.massart@student.hepl.be>
2023-05-15 11:54:25 +0200

    Added HubStack

---
+++ App/Pages/Hub.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Hub = () => {
+  return (
+    <View>
+      <Text>Hub page</Text>
+    </View>
+  );
+};
+
+export default Hub;
