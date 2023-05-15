1ea5d7f0275643f71b285e035dab24c3158474ef
JustinMassart <justin.massart@student.hepl.be>
2023-05-15 11:55:25 +0200

    Added new title in AppBar for main routes

--- App/Navigation/index.tsx
+++ App/Navigation/index.tsx
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useTranslation} from 'react-i18next';
 
+import HubStack from 'app/Navigation/AppTab/HubStack';
 import HomeStack from 'app/Navigation/AppTab/HomeStack';
 import BeersStack from 'app/Navigation/AppTab/BeersStack';
+import PlacesStack from 'app/Navigation/AppTab/PlacesStack';
+import ProfileStack from 'app/Navigation/AppTab/ProfileStack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import LanguageStack from './AppTab/LanguageStack';
 
 export type RootNavigationType = {
   App: undefined;
@@ -14,12 +17,12 @@ export type RootNavigationType = {
   Beers: undefined;
   Places: undefined;
   Profile: undefined;
-  Language: undefined;
 };
 
 const Tab = createBottomTabNavigator<RootNavigationType>();
 
 const Navigator = () => {
+  const {t} = useTranslation();
   const [initialRoute, setInitialRoute] = useState<'Home' | 'Language'>();
 
   useEffect(() => {
@@ -43,7 +46,7 @@ const Navigator = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName={initialRoute}
+        initialRouteName={'Home'}
         screenOptions={{
           // TODO: Remove detachPreviousScreen
           // https://github.com/react-navigation/react-navigation/issues/9891#issuecomment-916453157
@@ -52,9 +55,31 @@ const Navigator = () => {
           headerShown: false,
           presentation: 'modal',
         }}>
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Beers" component={BeersStack} />
-        <Tab.Screen name="Language" component={LanguageStack} />
+        <Tab.Screen
+          name="Hub"
+          component={HubStack}
+          options={{title: t('Hub.title') || 'Hub'}}
+        />
+        <Tab.Screen
+          name="Beers"
+          component={BeersStack}
+          options={{title: t('Beers.title') || 'Beers'}}
+        />
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{title: t('Home.title') || 'Home'}}
+        />
+        <Tab.Screen
+          name="Places"
+          component={PlacesStack}
+          options={{title: t('Places.title') || 'Places'}}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileStack}
+          options={{title: t('Profile.title') || 'Profile'}}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
