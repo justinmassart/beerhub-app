3f3ef611710baa65cb35a1bcaea34d2178fc595b
JustinMassart <justin.massart@student.hepl.be>
2023-05-22 17:55:56 +0200

    Removed AppTab content from index navigation

--- App/Navigation/index.tsx
+++ App/Navigation/index.tsx
@@ -3,29 +3,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useTranslation} from 'react-i18next';
 
-import HubStack from 'app/Navigation/AppTab/HubStack';
-import HomeStack from 'app/Navigation/AppTab/HomeStack';
-import BeersStack from 'app/Navigation/AppTab/BeersStack';
-import PlacesStack from 'app/Navigation/AppTab/PlacesStack';
-import ProfileStack from 'app/Navigation/AppTab/ProfileStack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AppTab from 'app/Navigation/AppTab';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import BrandsStack from 'app/Navigation/AppTab/BrandStack';
 
 export type RootNavigationType = {
-  App: undefined;
-  Home: undefined;
-  Hub: undefined;
-  Beers: undefined;
-  Places: undefined;
-  Profile: undefined;
+  AppTab: undefined;
+  Brands: undefined;
 };
 
-const Tab = createBottomTabNavigator<RootNavigationType>();
+const RootStack = createStackNavigator<RootNavigationType>();
 
 const Navigator = () => {
   const {t} = useTranslation();
-  const [initialRoute, setInitialRoute] = useState<'Home' | 'Language'>();
+  //const [initialRoute, setInitialRoute] = useState<'Home' | 'Language'>();
 
-  useEffect(() => {
+  /*   useEffect(() => {
     const checkStoredLocale = async () => {
       const storedLocale = await AsyncStorage.getItem('locale');
 
@@ -41,12 +35,11 @@ const Navigator = () => {
 
   if (initialRoute === undefined) {
     return null;
-  }
+  } */
 
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={'Home'}
+      <RootStack.Navigator
         screenOptions={{
           // TODO: Remove detachPreviousScreen
           // https://github.com/react-navigation/react-navigation/issues/9891#issuecomment-916453157
@@ -55,32 +48,9 @@ const Navigator = () => {
           headerShown: false,
           presentation: 'modal',
         }}>
-        <Tab.Screen
-          name="Hub"
-          component={HubStack}
-          options={{title: t('Hub.title') || 'Hub'}}
-        />
-        <Tab.Screen
-          name="Beers"
-          component={BeersStack}
-          options={{title: t('Beers.title') || 'Beers'}}
-        />
-        <Tab.Screen
-          name="Home"
-          component={HomeStack}
-          options={{title: t('Home.title') || 'Home'}}
-        />
-        <Tab.Screen
-          name="Places"
-          component={PlacesStack}
-          options={{title: t('Places.title') || 'Places'}}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileStack}
-          options={{title: t('Profile.title') || 'Profile'}}
-        />
-      </Tab.Navigator>
+        <RootStack.Screen name="AppTab" component={AppTab} />
+        <RootStack.Screen name="Brands" component={BrandsStack} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
