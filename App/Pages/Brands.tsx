b20c5f5f07cc11c203a7a692de9427d4dfdd871b
JustinMassart <justin.massart@student.hepl.be>
2023-05-22 17:54:59 +0200

    Added Brands page

---
+++ App/Pages/Brands.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Text from 'app/Components/Atoms/Text';
+import View from 'app/Components/Atoms/View';
+
+const Brands = () => {
+  return (
+    <View>
+      <View>
+        <Text>Liste des marques</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Brands;
