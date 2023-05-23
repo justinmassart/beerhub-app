fba22be6d6602dcee6d52dbd4a1935a7e7e1c5db
JustinMassart <justin.massart@student.hepl.be>
2023-05-23 14:45:35 +0200

    Updated index navigation

--- App/Navigation/index.tsx
+++ App/Navigation/index.tsx
@@ -1,17 +1,29 @@
 import React, {useEffect, useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, RouteProp} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useTranslation} from 'react-i18next';
 
 import AppTab from 'app/Navigation/AppTab';
-import {createStackNavigator} from '@react-navigation/stack';
+import {
+  StackNavigationProp,
+  createStackNavigator,
+} from '@react-navigation/stack';
 
 import BrandsStack from 'app/Navigation/AppTab/BrandStack';
 
 export type RootNavigationType = {
   AppTab: undefined;
-  Brands: undefined;
+  Brands: {
+    screen: 'brands' | 'brand';
+  };
 };
+
+export type RootStackNavigationProp = StackNavigationProp<RootNavigationType>;
+
+export type RootStackRouteProp<T extends keyof RootNavigationType> = RouteProp<
+  RootNavigationType,
+  T
+>;
 
 const RootStack = createStackNavigator<RootNavigationType>();
 
@@ -40,13 +52,13 @@ const Navigator = () => {
   return (
     <NavigationContainer>
       <RootStack.Navigator
+        initialRouteName="AppTab"
         screenOptions={{
           // TODO: Remove detachPreviousScreen
           // https://github.com/react-navigation/react-navigation/issues/9891#issuecomment-916453157
           detachPreviousScreen: false,
-          animationEnabled: false,
+          animationEnabled: true,
           headerShown: false,
-          presentation: 'modal',
         }}>
         <RootStack.Screen name="AppTab" component={AppTab} />
         <RootStack.Screen name="Brands" component={BrandsStack} />
