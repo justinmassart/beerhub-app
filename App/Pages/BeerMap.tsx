5c4c6373b8a848b6ef6e257d220419f8980914f8
JustinMassart <justin.massart@student.hepl.be>
2023-05-23 14:45:16 +0200

    Added BeerMap page

---
+++ App/Pages/BeerMap.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Text from 'app/Components/Atoms/Text';
+import View from 'app/Components/Atoms/View';
+
+const Beer = () => {
+  return (
+    <View>
+      <View>
+        <Text>Carte</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Beer;
