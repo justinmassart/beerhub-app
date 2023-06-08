7e089f1e868c661ffce11f6360ea86f6fe1ee61a
JustinMassart <justin.massart@student.hepl.be>
2023-06-08 12:25:08 +0200

    Added BeerHeader Molecules & Updated Beer page with BeerHeader

--- App/Pages/Beer.tsx
+++ App/Pages/Beer.tsx
@@ -1,28 +1,18 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
 import Text from 'app/Components/Atoms/Text';
 import View from 'app/Components/Atoms/View';
-import {RootStackNavigationProp} from 'app/Navigation';
-import {BeersStackNavigationProp} from 'app/Navigation/AppTab/BeersStack';
+import BeerHeader from 'app/Components/Molecules/BeerHeader';
 
 const Beer = () => {
-  const {params} = useRoute();
-  const {navigate} = useNavigation<RootStackNavigationProp>();
-  const {navigate: beerNavigation} = useNavigation<BeersStackNavigationProp>();
+  const { params } = useRoute();
+  const { beer } = params;
 
   return (
-    <View>
+    <View noPadding>
+      <BeerHeader beer={beer}></BeerHeader>
       <View>
-        <Text>{params?.beer?.name}</Text>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => navigate('Brands', {screen: 'brand'})}>
-          <Text>Val-Dieu</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => beerNavigation('beerMap')}>
-          <Text>Où déguster ?</Text>
-        </TouchableOpacity>
+        <Text>Test</Text>
       </View>
     </View>
   );
