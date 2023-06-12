8344dcf9afc2c7b930378e9c6eedf5031b1a8e5e
JustinMassart <justin.massart@student.hepl.be>
2023-06-12 17:04:46 +0200

    Updated Brand page with GET_BRAND query & display of brand_name

--- App/Pages/Brand.tsx
+++ App/Pages/Brand.tsx
@@ -1,15 +1,46 @@
-import {useRoute} from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { useRoute, RouteProp, ParamListBase } from '@react-navigation/native';
 import Text from 'app/Components/Atoms/Text';
 import View from 'app/Components/Atoms/View';
-import React from 'react';
+import GET_BRAND from 'app/Operations/queries/getBrand';
+
+interface RouteParams {
+  brandId?: string;
+}
 
 const Brand = () => {
-  const {params} = useRoute();
+  const { params } = useRoute();
+  const [brand, setBrand] = useState(null);
+
+  const getBrand = async (id: string) => {
+    try {
+      const response = await GET_BRAND(id);
+      setBrand(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const { brandId } = params as RouteParams;
+    if (brandId) {
+      getBrand(brandId);
+    }
+  }, [params]);
+
+  console.log(brand[0]);
+
   return (
     <View>
-      <View>
-        <Text>Une marque</Text>
-      </View>
+      {brand ? (
+        <View>
+          <Text>{brand[0]['name']}</Text>
+        </View>
+      ) : (
+        <View>
+          <Text>error</Text>
+        </View>
+      )}
     </View>
   );
 };
