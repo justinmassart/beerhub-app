fbd42227eca8d50eb3dfc54fdd66645292b8dcd6
JustinMassart <justin.massart@student.hepl.be>
2023-07-12 15:42:45 +0200

    Updated Settings Pages

--- App/Pages/Settings.tsx
+++ App/Pages/Settings.tsx
@@ -1,18 +1,21 @@
-/* eslint-disable react-native/no-inline-styles */
-import {useNavigation} from '@react-navigation/native';
-import {ProfileStackNavigationProp} from 'app/Navigation/AppTab/ProfileStack';
+import { useNavigation } from '@react-navigation/native';
+import { ProfileStackNavigationProp } from 'app/Navigation/AppTab/ProfileStack';
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 const Profile = () => {
-  const {navigate} = useNavigation<ProfileStackNavigationProp>();
+  const { navigate } = useNavigation<ProfileStackNavigationProp>();
   return (
     <View>
       <View>
         <Text>Setting page</Text>
       </View>
       <TouchableOpacity
-        style={{alignItems: 'center', backgroundColor: '#DDDDDD', padding: 10}}
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#DDDDDD',
+          padding: 10,
+        }}
         onPress={() => navigate('Language')}>
         <Text>Languages</Text>
       </TouchableOpacity>
