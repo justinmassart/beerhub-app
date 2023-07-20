d4b1cfec6c8ee1a583956caa0ce7314e85b56e45
JustinMassart <justin.massart@student.hepl.be>
2023-07-20 17:09:42 +0200

    Added success message for account creation & go back to home button

--- App/Pages/SignUp.tsx
+++ App/Pages/SignUp.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 import View from 'app/Components/Atoms/View';
 import Text from 'app/Components/Atoms/Text';
 import PageContainer from 'app/Components/Atoms/PageContainer';
+
 import SignUpForm from 'app/Forms/SingUpForm';
+
+import { RootStackNavigationProp } from 'app/Navigation';
 
 const SignUp = () => {
   const [userEmail, setUserEmail] = useState<string>('');
+
+  const navigation = useNavigation<RootStackNavigationProp>();
+
+  const goToHomePage = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'AppTab' }],
+    });
+  };
 
   return (
     <PageContainer>
@@ -16,12 +30,21 @@ const SignUp = () => {
           <Text>Sign Up page</Text>
         </View>
         {userEmail ? (
-          <View>
-            <Text>Your account was successfully created !</Text>
-            <Text>
-              To verify your email address, click on the button in the email to
-              this address : {userEmail}
-            </Text>
+          <View noPaddingHorizontal>
+            <View noPaddingHorizontal>
+              <Text>Your account was successfully created !</Text>
+              <Text>
+                To verify your email address, click on the button in the email
+                to this address : {userEmail}
+              </Text>
+            </View>
+            <View noPaddingHorizontal>
+              <TouchableOpacity
+                style={{ backgroundColor: 'white', padding: 10 }}
+                onPress={goToHomePage}>
+                <Text>Go to the Home page</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         ) : (
           <View noPaddingHorizontal>
