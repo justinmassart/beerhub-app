26f720964e26f358c3da8f36d372959f2f51fc14
JustinMassart <justin.massart@student.hepl.be>
2023-07-20 17:09:15 +0200

    Added loader while submitting for sign up form

--- App/Forms/SingUpForm.tsx
+++ App/Forms/SingUpForm.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, ActivityIndicator } from 'react-native';
 
 import View from 'app/Components/Atoms/View';
 import Text from 'app/Components/Atoms/Text';
@@ -9,8 +9,26 @@ import REGISTER_USER from 'app/Operations/queries/registerUser';
 
 import availableCountries from 'app/Helpers/availableCountries';
 
+const Loader = () => (
+  <View
+    style={{
+      zIndex: 1,
+      flex: 1,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+    <ActivityIndicator size="large" color={'#FC6701'} />
+  </View>
+);
+
 const SignUpForm = ({ userEmail }) => {
   const [isFormComplete, setIsFormComplete] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [formData, setFormData] = useState({
     firstname: '',
     lastname: '',
@@ -23,11 +41,14 @@ const SignUpForm = ({ userEmail }) => {
 
   const handleSubmit = async () => {
     try {
+      setIsLoading(true);
       const response = await REGISTER_USER(formData);
       console.log(response);
+      setIsLoading(false);
       userEmail(formData.email);
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
       userEmail('');
     }
   };
@@ -50,78 +71,89 @@ const SignUpForm = ({ userEmail }) => {
 
   return (
     <>
-      <View noPaddingHorizontal>
-        <Text>Login Form</Text>
-      </View>
-      <View noPaddingHorizontal>
-        <Text>First name</Text>
-        <InputField
-          type="text"
-          value={formData.firstname}
-          onChangeText={value => setFormData({ ...formData, firstname: value })}
-        />
-      </View>
-      <View noPaddingHorizontal>
-        <Text>Last name</Text>
-        <InputField
-          type="text"
-          value={formData.lastname}
-          onChangeText={value => setFormData({ ...formData, lastname: value })}
-        />
-      </View>
-      <View noPaddingHorizontal>
-        <Text>Username</Text>
-        <InputField
-          type="text"
-          value={formData.username}
-          onChangeText={value => setFormData({ ...formData, username: value })}
-        />
-      </View>
-      <View noPaddingHorizontal>
-        <Text>Email</Text>
-        <InputField
-          type="email"
-          value={formData.email}
-          onChangeText={value => setFormData({ ...formData, email: value })}
-        />
-      </View>
-      <View noPaddingHorizontal>
-        <Text>Country</Text>
-        <InputField
-          type="text"
-          value={formData.country}
-          onChangeText={value => setFormData({ ...formData, country: value })}
-        />
-      </View>
-      <View noPaddingHorizontal>
-        <Text>Password</Text>
-        <InputField
-          type="password"
-          value={formData.password}
-          onChangeText={value => setFormData({ ...formData, password: value })}
-        />
-      </View>
-      <View noPaddingHorizontal>
-        <Text>Confirm password</Text>
-        <InputField
-          type="password"
-          value={formData.confirm_password}
-          onChangeText={value =>
-            setFormData({ ...formData, confirm_password: value })
-          }
-        />
-      </View>
-      <View noPaddingHorizontal>
-        <TouchableOpacity
-          disabled={!isFormComplete}
-          style={{
-            backgroundColor: 'white',
-            padding: 10,
-            opacity: !isFormComplete ? 0.4 : 1,
-          }}
-          onPress={handleSubmit}>
-          <Text>Create my account</Text>
-        </TouchableOpacity>
+      {isLoading && <Loader />}
+      <View noPadding style={{ opacity: isLoading ? 0.25 : 1 }}>
+        <View noPaddingHorizontal>
+          <Text>Login Form</Text>
+        </View>
+        <View noPaddingHorizontal>
+          <Text>First name</Text>
+          <InputField
+            type="text"
+            value={formData.firstname}
+            onChangeText={value =>
+              setFormData({ ...formData, firstname: value })
+            }
+          />
+        </View>
+        <View noPaddingHorizontal>
+          <Text>Last name</Text>
+          <InputField
+            type="text"
+            value={formData.lastname}
+            onChangeText={value =>
+              setFormData({ ...formData, lastname: value })
+            }
+          />
+        </View>
+        <View noPaddingHorizontal>
+          <Text>Username</Text>
+          <InputField
+            type="text"
+            value={formData.username}
+            onChangeText={value =>
+              setFormData({ ...formData, username: value })
+            }
+          />
+        </View>
+        <View noPaddingHorizontal>
+          <Text>Email</Text>
+          <InputField
+            type="email"
+            value={formData.email}
+            onChangeText={value => setFormData({ ...formData, email: value })}
+          />
+        </View>
+        <View noPaddingHorizontal>
+          <Text>Country</Text>
+          <InputField
+            type="text"
+            value={formData.country}
+            onChangeText={value => setFormData({ ...formData, country: value })}
+          />
+        </View>
+        <View noPaddingHorizontal>
+          <Text>Password</Text>
+          <InputField
+            type="password"
+            value={formData.password}
+            onChangeText={value =>
+              setFormData({ ...formData, password: value })
+            }
+          />
+        </View>
+        <View noPaddingHorizontal>
+          <Text>Confirm password</Text>
+          <InputField
+            type="password"
+            value={formData.confirm_password}
+            onChangeText={value =>
+              setFormData({ ...formData, confirm_password: value })
+            }
+          />
+        </View>
+        <View noPaddingHorizontal>
+          <TouchableOpacity
+            disabled={!isFormComplete}
+            style={{
+              backgroundColor: 'white',
+              padding: 10,
+              opacity: !isFormComplete ? 0.4 : 1,
+            }}
+            onPress={handleSubmit}>
+            <Text>Create my account</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   );
