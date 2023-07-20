4868dcad2e03ff31067b34bfed53950c935280df
JustinMassart <justin.massart@student.hepl.be>
2023-07-20 16:17:21 +0200

    Added success message display condition

--- App/Pages/SignUp.tsx
+++ App/Pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 
 import View from 'app/Components/Atoms/View';
@@ -7,15 +7,27 @@ import PageContainer from 'app/Components/Atoms/PageContainer';
 import SignUpForm from 'app/Forms/SingUpForm';
 
 const SignUp = () => {
+  const [userEmail, setUserEmail] = useState<string>('');
+
   return (
     <PageContainer>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View noPaddingHorizontal>
           <Text>Sign Up page</Text>
         </View>
-        <View noPaddingHorizontal>
-          <SignUpForm />
-        </View>
+        {userEmail ? (
+          <View>
+            <Text>Your account was successfully created !</Text>
+            <Text>
+              To verify your email address, click on the button in the email to
+              this address : {userEmail}
+            </Text>
+          </View>
+        ) : (
+          <View noPaddingHorizontal>
+            <SignUpForm userEmail={(value: string) => setUserEmail(value)} />
+          </View>
+        )}
       </ScrollView>
     </PageContainer>
   );
