44c74e4cb8c11e8bb0d122196c5f9bf850971de8
JustinMassart <justin.massart@student.hepl.be>
2023-07-24 16:51:22 +0200

    Updated display if login successful

--- App/Pages/Login.tsx
+++ App/Pages/Login.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { RootStackNavigationProp } from 'app/Navigation';
 
 import View from 'app/Components/Atoms/View';
 import Text from 'app/Components/Atoms/Text';
-import LoginForm from 'app/Forms/LoginForm';
 import PageContainer from 'app/Components/Atoms/PageContainer';
 
+import LoginForm from 'app/Forms/LoginForm';
+
+import { UserType } from 'app/Types/UserType';
+
 const Login = () => {
+  const [user, setUser] = useState<UserType | null>(null);
+
+  const navigation = useNavigation<RootStackNavigationProp>();
+
+  const goToHomePage = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'AppTab' }],
+    });
+  };
+
   return (
     <PageContainer>
       <View noPaddingHorizontal>
         <Text>Login page</Text>
       </View>
-      <LoginForm />
+      {user ? (
+        <View noPaddingHorizontal>
+          <Text>Welcome {user.firstname}</Text>
+          <Text>You are logged in</Text>
+          <TouchableOpacity
+            style={{ backgroundColor: 'white', padding: 10 }}
+            onPress={goToHomePage}>
+            <Text>Go to the Home page</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <LoginForm user={(value: UserType) => setUser(value)} />
+      )}
     </PageContainer>
   );
 };
