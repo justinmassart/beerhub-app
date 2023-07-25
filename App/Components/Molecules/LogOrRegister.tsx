d7acd1e2a4dc9e9e8262783f7c673b6e0ebb8da5
JustinMassart <justin.massart@student.hepl.be>
2023-07-25 19:00:34 +0200

    Added LogOrRegister molecule

---
+++ App/Components/Molecules/LogOrRegister.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Text from 'app/Components/Atoms/Text';
+import View from 'app/Components/Atoms/View';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import { ProfileStackNavigationProp } from 'app/Navigation/AppTab/ProfileStack';
+
+const LogOrRegister = () => {
+  const { navigate } = useNavigation<ProfileStackNavigationProp>();
+
+  return (
+    <View noPaddingHorizontal>
+      <Text>You are not logged in ...</Text>
+      <View noPaddingHorizontal>
+        <TouchableOpacity
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#DDDDDD',
+            padding: 10,
+          }}
+          onPress={() => navigate('Login')}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <Text>or</Text>
+        <TouchableOpacity
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#DDDDDD',
+            padding: 10,
+          }}
+          onPress={() => navigate('SignUp')}>
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default LogOrRegister;
