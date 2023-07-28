900b628c6933f9650448abcd59c1597e816239cd
JustinMassart <justin.massart@student.hepl.be>
2023-07-28 17:15:56 +0200

    Updated with VerifyPhoneNumberForm

--- App/Pages/Register.tsx
+++ App/Pages/Register.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native';
 
 import View from 'app/Components/Atoms/View';
 import Text from 'app/Components/Atoms/Text';
@@ -9,19 +7,17 @@ import PageContainer from 'app/Components/Atoms/PageContainer';
 
 import RegisterForm from 'app/Forms/RegisterForm';
 
-import { RootStackNavigationProp } from 'app/Navigation';
+import VerifyPhoneNumberForm from 'app/Forms/VerifyPhoneNumberForm';
 
 const Register = () => {
   const [userEmail, setUserEmail] = useState<string>('');
 
-  const navigation = useNavigation<RootStackNavigationProp>();
-
-  const goToHomePage = () => {
+  /*   const goToHomePage = () => {
     navigation.reset({
       index: 0,
       routes: [{ name: 'AppTab' }],
     });
-  };
+  }; */
 
   return (
     <PageContainer>
@@ -30,26 +26,7 @@ const Register = () => {
           <Text>Register page</Text>
         </View>
         {userEmail ? (
-          <View noPaddingHorizontal>
-            <View noPaddingHorizontal>
-              <Text>Your account was successfully created !</Text>
-              <Text>
-                To verify your email address, click on the button in the email
-                to this address : {userEmail}
-              </Text>
-              <Text>
-                After that, go to the login page in the profile page and log
-                yourself in !
-              </Text>
-            </View>
-            <View noPaddingHorizontal>
-              <TouchableOpacity
-                style={{ backgroundColor: 'white', padding: 10 }}
-                onPress={goToHomePage}>
-                <Text>Go to the Home page</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+          <VerifyPhoneNumberForm />
         ) : (
           <View noPaddingHorizontal>
             <RegisterForm userEmail={(value: string) => setUserEmail(value)} />
