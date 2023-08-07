83246539174a263b6e266e0d9c5f45d0d2dd7e77
JustinMassart <justin.massart@student.hepl.be>
2023-08-07 15:44:18 +0200

    Added Authorization header with authToken in revokeToken.ts

--- App/Operations/queries/revokeToken.ts
+++ App/Operations/queries/revokeToken.ts
@@ -3,18 +3,24 @@ import axiosInstance from 'app/Providers/Axiosinstance';
 
 const { BACKEND_URL } = Config;
 
-const REVOKE_TOKEN = async (id: string) => {
-  console.log(id);
+const REVOKE_TOKEN = async (id: string, authToken: string) => {
   if (id) {
     try {
-      const response = await axiosInstance.post(`${BACKEND_URL}/logout`, {
-        id,
-      });
+      const response = await axiosInstance.post(
+        `${BACKEND_URL}/logout`,
+        { id },
+        { headers: { Authorization: `Bearer ${authToken}` } },
+      );
       const data = response.data;
       console.log(data);
       return data;
-    } catch (error) {
-      throw error;
+    } catch (error: any) {
+      if (error.response && error.response.data && error.response.data.ERROR) {
+        const errorMessage = error.response.data.ERROR;
+        throw new Error(errorMessage);
+      } else {
+        throw new Error('UNKNOWN_ERROR');
+      }
     }
   }
 };
