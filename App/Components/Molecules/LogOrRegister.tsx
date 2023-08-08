982555c8389ef47519136b1eab4e58f77c4df73f
JustinMassart <justin.massart@student.hepl.be>
2023-08-08 18:00:58 +0200

    Updated / To fix

--- App/Components/Molecules/LogOrRegister.tsx
+++ App/Components/Molecules/LogOrRegister.tsx
@@ -5,20 +5,28 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { ProfileStackNavigationProp } from 'app/Navigation/AppTab/ProfileStack';
 
-const LogOrRegister = () => {
-  const { navigate } = useNavigation<ProfileStackNavigationProp>();
+const LogOrRegister = ({ displayMessage, onClose }) => {
+  const { navigate } = useNavigation();
 
   return (
-    <View noPaddingHorizontal>
-      <Text>You are not logged in ...</Text>
-      <View noPaddingHorizontal>
+    <View noPaddingHorizontal alignItems="center">
+      {displayMessage && <Text>You are not logged in ...</Text>}
+      <View noPaddingHorizontal alignItems="center">
         <TouchableOpacity
           style={{
             alignItems: 'center',
             backgroundColor: '#DDDDDD',
             padding: 10,
           }}
-          onPress={() => navigate('Login')}>
+          onPress={() => {
+            navigate('AppTab', {
+              screen: 'MyProfile',
+              params: {
+                screen: 'Login',
+              },
+            });
+            onClose(onClose);
+          }}>
           <Text>Login</Text>
         </TouchableOpacity>
         <Text>or</Text>
@@ -28,7 +36,15 @@ const LogOrRegister = () => {
             backgroundColor: '#DDDDDD',
             padding: 10,
           }}
-          onPress={() => navigate('Register')}>
+          onPress={() => {
+            navigate('AppTab', {
+              screen: 'MyProfile',
+              params: {
+                screen: 'Register',
+              },
+            });
+            onClose(onClose);
+          }}>
           <Text>Register</Text>
         </TouchableOpacity>
       </View>
