188a7692fca9a7ec6bbdcb452901457725757d44
JustinMassart <justin.massart@student.hepl.be>
2023-08-12 17:01:27 +0200

    Added getBrandsInputs query to research brands in backend

---
+++ App/Operations/queries/getBrandsInputs.ts
@@ -0,0 +1,26 @@
+import axiosInstance from 'app/Providers/Axiosinstance';
+import Config from 'react-native-config';
+
+const { BACKEND_URL } = Config;
+
+const GET_BRANDS_INPUTS = async (searchTerm: string) => {
+  if (searchTerm) {
+    try {
+      const response = await axiosInstance.get(
+        `${BACKEND_URL}/inputs/brand-selector?search_term=${searchTerm}`,
+      );
+      return response;
+    } catch (error: any) {
+      console.log(error);
+      if (error.response && error.response.data && error.response.data.ERROR) {
+        const errorMessage = error.response.data.ERROR;
+        console.log(errorMessage);
+        throw new Error(errorMessage);
+      } else {
+        throw new Error('UNKNOWN_ERROR');
+      }
+    }
+  }
+};
+
+export default GET_BRANDS_INPUTS;
