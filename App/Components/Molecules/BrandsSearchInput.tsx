a36129f707e5d2ce9cf9d5e693726bed24ff7c4a
JustinMassart <justin.massart@student.hepl.be>
2023-08-12 17:00:43 +0200

    Added BrandsSearchInput for selecting a brand in AddBeerForm

---
+++ App/Components/Molecules/BrandsSearchInput.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { TextInput, Text, TouchableOpacity } from 'react-native';
+
+import View from 'app/Components/Atoms/View';
+
+import BrandsSearchModal from 'app/Components/Molecules/Modals/BrandsSearchModals';
+import Box from '../Atoms/Box';
+
+const BrandsSearchInput = ({ brandId }) => {
+  const [isModalVisible, setModalVisible] = useState<boolean>(false);
+  const [selectedBrand, setSelectedBrand] = useState<{
+    id: string;
+    name: string;
+  } | null>(null);
+
+  const openModal = () => setModalVisible(true);
+  const closeModal = () => setModalVisible(false);
+  const handleSelectBrand = (brand: { id: string; name: string }) => {
+    setSelectedBrand(brand);
+    console.log(brand.id);
+    brandId(brand.id);
+    closeModal();
+  };
+
+  return (
+    <View noPaddingHorizontal>
+      <TouchableOpacity onPress={openModal}>
+        <Box input={true}>
+          <View noPaddingVertical flex={1} justifyContent="center">
+            <Text>{selectedBrand?.name || 'Select a Brand'}</Text>
+          </View>
+        </Box>
+      </TouchableOpacity>
+      <BrandsSearchModal
+        isVisible={isModalVisible}
+        onClose={closeModal}
+        onSelect={handleSelectBrand}
+      />
+    </View>
+  );
+};
+
+export default BrandsSearchInput;
